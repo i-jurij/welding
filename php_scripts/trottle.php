06a8fcdfa911e463b32588ce1c5e30f00c2ec735
IJurij <yjurij@gmail.com>
2024-04-08 20:02:45 +0300

    ch trottle

--- php_scripts/trottle.php
+++ php_scripts/trottle.php
@@ -5,15 +5,18 @@ $remote_ip = $_SERVER['REMOTE_ADDR'];
  * function count how many time user reload page by method post 
  * @return array with count and first time of sending form by ip ['ip' => ['count' => count, 'time' => first_time]]
  */
-function countPost($remote_ip) {
+function countPost($remote_ip)
+{
     // счетчик $_SESSION[$remote_ip] = ['count' => number, 'time' => time_of_trottle]
     // чтобы проверять время ожидания пользователя перед отправкой следующего запроса
     // время ожидания - 15 минут
     // счетчик отправки с одного ip 
-    if (isset($_SESSION[$remote_ip])) {
-        $_SESSION[$remote_ip]['count'] = $_SESSION[$remote_ip]['count'] + 1;
-    } else {
-        $_SESSION[$remote_ip] = ['count' => 0, 'time' => time()];
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        if (isset($_SESSION[$remote_ip])) {
+            $_SESSION[$remote_ip]['count'] = $_SESSION[$remote_ip]['count'] + 1;
+        } else {
+            $_SESSION[$remote_ip] = ['count' => 0, 'time' => time()];
+        }
     }
     return $_SESSION[$remote_ip];
 }
@@ -29,7 +32,7 @@ function preventTrottle($session_remote_ip)
     // waiting_time 900 = 15 min X 60 sec
     $waiting_time = 900;
     $now_time = time();
-    
+
     $diff = $now_time - $first_time;
 
     if ($count < 3 || ($count >= 3 && $diff > $waiting_time)) {
