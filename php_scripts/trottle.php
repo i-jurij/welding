a615bd99a749937b5f72eefcc92a847ffd555359
IJurij <yjurij@gmail.com>
2024-04-12 18:58:52 +0300

    check ip in trottle

--- php_scripts/trottle.php
+++ php_scripts/trottle.php
@@ -1,5 +1,13 @@
 <?php
-$remote_ip = $_SERVER['REMOTE_ADDR'];
+
+function isValidIpAddress($ip)
+{
+    if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4 | FILTER_FLAG_IPV6 | FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE) === false) {
+        return false;
+    }
+    return true;
+}
+$remote_ip = isValidIpAddress($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : '';
 
 /**
  * function count how many time user reload page by method post 
@@ -11,14 +19,16 @@ function countPost($remote_ip)
     // чтобы проверять время ожидания пользователя перед отправкой следующего запроса
     // время ожидания - 15 минут
     // счетчик отправки с одного ip 
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if ($_SERVER['REQUEST_METHOD'] === 'POST' && $remote_ip !== '') {
         if (isset($_SESSION[$remote_ip])) {
             $_SESSION[$remote_ip]['count'] = $_SESSION[$remote_ip]['count'] + 1;
         } else {
             $_SESSION[$remote_ip] = ['count' => 0, 'time' => time()];
         }
-        return $_SESSION[$remote_ip];
+    } else {
+        $_SESSION[$remote_ip] = false;
     }
+    return $_SESSION[$remote_ip];
 }
 /**
  * function for trottle prevent
@@ -27,16 +37,20 @@ function countPost($remote_ip)
  */
 function preventTrottle($session_remote_ip)
 {
-    $count = $session_remote_ip['count'];
-    $first_time = $session_remote_ip['time'];
-    // waiting_time 900 = 15 min X 60 sec
-    $waiting_time = 900;
-    $now_time = time();
-
-    $diff = $now_time - $first_time;
-
-    if ($count < 3 || ($count >= 3 && $diff > $waiting_time)) {
-        return true;
+    if ($session_remote_ip !== false) {
+        $count = $session_remote_ip['count'];
+        $first_time = $session_remote_ip['time'];
+        // waiting_time 900 = 15 min X 60 sec
+        $waiting_time = 900;
+        $now_time = time();
+    
+        $diff = $now_time - $first_time;
+    
+        if ($count < 3 || ($count >= 3 && $diff > $waiting_time)) {
+            return true;
+        } else {
+            return false;
+        }
     } else {
         return false;
     }
